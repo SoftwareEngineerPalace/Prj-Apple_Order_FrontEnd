e4682e8b3cd9d450350116aaf148a01b8a77d67d
xiaojianjun <jianjun.xiao@foxmail.com>
2024-02-09 09:38:04 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import "./App.less";
+import type { TableColumnsType } from "antd";
+import { Image, Table, Card } from "antd";
+import { useEffect } from "react";
+import queryString from "query-string";
+import { Order, LineItem } from "./model";
+const hostname = "localhost";
+
+const App = () => {
+  const [forceFresh, setForceFresh] = useState(false);
+  const [orderList, setOrderList] = useState<Array<Order>>([]);
+  const [subOrderListMap, setSubOrderListMap] = useState<{
+    [orderId: string]: LineItem[];
+  }>({});
+
+  // 获取订单列表
+  useEffect(() => {
+    const getOrderList = async () => {
+      const { data } = await fetch(
+        `http://${hostname}:3001/order/getOrderList`
+      ).then((response) => response.json());
+      setOrderList(data);
+    };
+    getOrderList();
+  }, []);
+
+  /** 根据订单 id 获取商品列表 */
+  const getLineItemList = React.useCallback(
+    async (orderId: string) => {
+      const paramStr = queryString.stringify({ orderId });
+      const { data } = await fetch(
+        `http://${hostname}:3001/order/getLineItemByOrderId?${paramStr}`
+      ).then((response) => response.json());
+      let map: { [key: string]: LineItem[] } = subOrderListMap;
+      map[orderId] = data;
+      setSubOrderListMap(map);
+      setForceFresh(!forceFresh);
+    },
+    [subOrderListMap, forceFresh]
+  );
+
+  const expandedRowRender = React.useCallback(
+    ({ id, image }: any) => {
+      const columns: TableColumnsType<LineItem> = [
+        { title: "Order ID", dataIndex: "orderId", key: "orderId" },
+        { title: "ID", dataIndex: "id", key: "id" },
+        { title: "Item", dataIndex: "item", key: "item" },
+        { title: "Profit", dataIndex: "profit", key: "profit" },
+        { title: "Sales", dataIndex: "sales", key: "sales" },
+      ];
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <Table
+            columns={columns}
+            dataSource={subOrderListMap[id]}
+            pagination={false}
+            rowKey={"id"}
+            locale={{
+              emptyText: (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <p style={{ marginBottom: 0 }}>no items</p>
+                </div>
+              ),
+            }}
+          />
+          {!!image ? (
+            <Image
+              width={200}
+              height={150}
+              src={`/assets/${image}`}
+            ></Image>
+          ) : (
+            <span>no image</span>
+          )}
+        </div>
+      );
+    },
+    [subOrderListMap]
+  );
+
+  const columns: TableColumnsType<Order> = [
+    { title: "ID", dataIndex: "id" },
+    { title: "Category", dataIndex: "category", key: "category" },
+    { title: "Sub-Category", dataIndex: "subCategory", key: "subCategory" },
+    { title: "Segment", dataIndex: "segment", key: "segment" },
+    { title: "Product Name", dataIndex: "productName", key: "productName" },
+    { title: "Order Date", dataIndex: "orderDate", key: "orderDate" },
+    { title: "Region", dataIndex: "region", key: "region" },
+    { title: "Profit", dataIndex: "profit", key: "profit" },
+    { title: "Sales", dataIndex: "sales", key: "sales" },
+  ];
+
+  return (
+    <div>
+      <Card>
+        <Table
+          columns={columns}
+          expandable={{
+            expandedRowRender,
+            expandRowByClick: true,
+          }}
+          dataSource={orderList}
+          rowKey="id"
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                if (!subOrderListMap?.[record.id]) {
+                  getLineItemList(record.id);
+                }
+              },
+            };
+          }}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/model.ts
@@ -0,0 +1,71 @@
+/**
+ * Order
+ */
+interface Order {
+  /**
+   * 商品类型
+   */
+  category?: string;
+  /**
+   * 订单 id
+   */
+  id: string;
+  /**
+   * 下单时间
+   */
+  orderDate?: string;
+  /**
+   * 产品名称
+   */
+  productName?: string;
+  /**
+   * 利润
+   */
+  profit?: string;
+  /**
+   * 部门所在区域
+   */
+  region?: string;
+  /**
+   * 销售额
+   */
+  sales?: string;
+  /**
+   * 部门
+   */
+  segment?: string;
+  /**
+   * 商品子类型
+   */
+  subCategory?: string;
+  [property: string]: any;
+}
+
+/**
+ * 商品
+ */
+interface LineItem {
+    /**
+     * 商品 id
+     */
+    id: string;
+    /**
+     * 商品名
+     */
+    item?: string;
+    /**
+     * 商品所属的订单号
+     */
+    orderId?: string;
+    /**
+     * 利润
+     */
+    profit?: string;
+    /**
+     * 销售额
+     */
+    sales?: string;
+    [property: string]: any;
+}
+
+export { Order, LineItem };
