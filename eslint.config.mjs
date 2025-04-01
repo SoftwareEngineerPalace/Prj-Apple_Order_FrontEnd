2129bcc0742ac575eb1d2da59a0cb1548bcd9bd8
肖建军 <jianjun.xiao@foxmail.com>
2025-04-01 20:27:53 +0800

    format: 格式化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,37 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from 'eslint-config-prettier'; 
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: "detect", // 自动检测 React 版本
-      },
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        languageOptions: { globals: globals.browser },
     },
-  },
-  {
-    ignores: [
-      "node_modules",
-      "src/.umi",
-      "src/.umi-production",
-      "dist",
-      "public",
-      "package-lock.json",
-      "build",
-    ],
-  },
-  { rules: { "@typescript-eslint/no-explicit-any": "off" } },
-  eslintConfigPrettier,
-  {
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+    },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        settings: {
+            react: {
+                version: 'detect', // 自动检测 React 版本
+            },
+        },
+    },
+    {
+        ignores: ['node_modules', 'src/.umi', 'src/.umi-production', 'dist', 'public', 'package-lock.json', 'build'],
+    },
+    { rules: { '@typescript-eslint/no-explicit-any': 'off' } },
+    eslintConfigPrettier,
+    {
         plugins: {
             prettier: eslintPluginPrettier,
         },
@@ -48,5 +40,5 @@ export default defineConfig([
             'arrow-body-style': 'off',
             'prefer-arrow-callback': 'off',
         },
-  },
+    },
 ]);
--- src/model.ts
+++ src/model.ts
@@ -2,43 +2,43 @@
  * Order
  */
 interface Order {
-  /**
-   * 商品类型
-   */
-  category?: string;
-  /**
-   * 订单 id
-   */
-  id: string;
-  /**
-   * 下单时间
-   */
-  orderDate?: string;
-  /**
-   * 产品名称
-   */
-  productName?: string;
-  /**
-   * 利润
-   */
-  profit?: string;
-  /**
-   * 部门所在区域
-   */
-  region?: string;
-  /**
-   * 销售额
-   */
-  sales?: string;
-  /**
-   * 部门
-   */
-  segment?: string;
-  /**
-   * 商品子类型
-   */
-  subCategory?: string;
-  [property: string]: any;
+    /**
+     * 商品类型
+     */
+    category?: string;
+    /**
+     * 订单 id
+     */
+    id: string;
+    /**
+     * 下单时间
+     */
+    orderDate?: string;
+    /**
+     * 产品名称
+     */
+    productName?: string;
+    /**
+     * 利润
+     */
+    profit?: string;
+    /**
+     * 部门所在区域
+     */
+    region?: string;
+    /**
+     * 销售额
+     */
+    sales?: string;
+    /**
+     * 部门
+     */
+    segment?: string;
+    /**
+     * 商品子类型
+     */
+    subCategory?: string;
+    [property: string]: any;
 }
 
 /**
