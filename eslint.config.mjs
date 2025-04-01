a387e337917be3a04ec40cc5add8846cbbcb88fd
肖建军 <jianjun.xiao@foxmail.com>
2025-04-01 20:42:13 +0800

    fix: test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,13 +7,13 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx,json}'] },
     {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx,json}'],
         languageOptions: { globals: globals.browser },
     },
     {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx,json}'],
         plugins: { js },
         extends: ['js/recommended'],
     },
