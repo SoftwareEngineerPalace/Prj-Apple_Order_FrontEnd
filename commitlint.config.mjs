5eef78fe7209d0361bdb1d2a1f3e8ca7c30f60bd
肖建军 <jianjun.xiao@foxmail.com>
2025-04-01 20:25:52 +0800

    fix: format

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,25 +1,10 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "build",
-        "ci",
-        "chore",
-        "revert",
-        "format",
-        "comment",
-        "log",
-      ],
-    ],
-  },
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert', 'format', 'comment', 'log'],
+        ],
+    },
 };
